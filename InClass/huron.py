528a449c20685c3483369f631eb0ccee5d255b80
S3bastianRom <joansebastian85@hotmail.com>
2025-02-25 21:15:28 -0500

    Unnecessary files

--- InClass/huron.py
+++
@@ -1,8 +0,0 @@
-from animal_exotico import Animal_Exotico
-
-class Huron(Animal_Exotico):
-    def __init__(self, nombre:str, peso:float, edad:int, pais_origen:str, impuestos: float):
-        super().__init__(nombre, peso, edad, pais_origen, impuestos)
-
-    def hacer_sonido(self):
-        return "¡Eek Eek!"
